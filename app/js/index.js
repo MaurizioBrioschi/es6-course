b83c4d156fa118343b9926dde9e93269c4e707df
Maurizio Brioschi <maurizio.brioschi@limango.de>
2018-02-25 19:20:10 +0100

    export/import examples

---
+++ app/js/index.js
@@ -0,0 +1,13 @@
+/* styling */
+require('styles/main.scss');
+/* js */
+import $ from 'jquery';
+import { log, logTitle } from 'logger';
+/* your imports */
+/* coding examples */
+
+
+log(hello());
+log(powers);
+log(add(100,100));
+log(milesToKm(500));
